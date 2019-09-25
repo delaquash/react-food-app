a784b5e82ca80e39ed967cb0af8fb364326c1546
Olaide <olaidemmanuel1@gmail.com>
2019-09-25 08:56:54 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const KEY_ID = '3a0d08a1';
+const API_KEY = 'd1600a2325b7a888a81c607acbc58891';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
