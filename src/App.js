aae74334411581a865e6352786c7ac314f0c2bdd
Olaide <olaidemmanuel1@gmail.com>
2019-09-25 14:49:35 +0100

    Worked on the styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Recipe from './Recipe';
+import { async } from 'q';
 
-const KEY_ID = '3a0d08a1';
-const API_KEY = 'd1600a2325b7a888a81c607acbc58891';
 
 const App = () => {
+const APP_ID = '3a0d08a1';
+const APP_KEY = 'd1600a2325b7a888a81c607acbc58891';
+
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState("");
+const [query, setQuery] = useState('')
+
+
+useEffect(() => {
+getRecipes();
+}, [query]);
+
+const getRecipes = async () => {
+  const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+
+    setRecipes(data.hits)
+    // console.log(data);
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+}
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form" onSubmit={getSearch}>
+        <input className="search-bar" type="text"
+          value={search} onChange={updateSearch}
+          placeholder="Enter recipe name"
+        />
+        <button className="search-button"
+        type="submit">
+        Search
+        </button>
+      </form>
+        <div className="recipe">
+          {recipes.map(recipe =>
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        )};
+      </div>
     </div>
   );
 }
