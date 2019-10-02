2d6c682ce2ca80431d88a3215079ad67078f7e80
Olaide <olaidemmanuel1@gmail.com>
2019-10-02 11:27:45 +0100

    Updates

---
+++ precache-manifest.1431eb052a7481dd52eaae61250d66e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f338b221de8bf71264ed33b05e5815",
+    "url": "/react-food-app/index.html"
+  },
+  {
+    "revision": "c073c130e2aa02d6a259",
+    "url": "/react-food-app/static/css/main.38d69cd5.chunk.css"
+  },
+  {
+    "revision": "8b1548e3c2cd878c4798",
+    "url": "/react-food-app/static/js/2.6e8915e4.chunk.js"
+  },
+  {
+    "revision": "c073c130e2aa02d6a259",
+    "url": "/react-food-app/static/js/main.5a10e2c8.chunk.js"
+  },
+  {
+    "revision": "04db58507184c26fa460",
+    "url": "/react-food-app/static/js/runtime-main.ccd652a5.js"
+  }
+]);
